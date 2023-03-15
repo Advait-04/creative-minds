3d509d1b0153a1bc98a11843d7f06ca2a9a20c11
Advait Suresh <advaitsuresh2002@gmail.com>
2023-03-15 21:10:11 +0530

    add post

---
+++ components/Nav.js
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import { auth } from "../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Image from "next/image";
+
+export default function Nav() {
+    const [user, loading] = useAuthState(auth);
+    console.log(user);
+    return (
+        <nav className="flex justify-between items-center py-10 px-10">
+            <Link href={"/"}>
+                <button className="text-lg font-medium">Creative Minds</button>
+            </Link>
+            <ul className="flex items-center gap-10">
+                {!user && (
+                    <Link href={"/auth/login"} legacyBehavior>
+                        <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">
+                            Join Now
+                        </a>
+                    </Link>
+                )}
+
+                {user && (
+                    <div className="flex items-center gap-6">
+                        <Link href={"/post"}>
+                            <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-mg text-sm">
+                                Post
+                            </button>
+                        </Link>
+                        <Link href={"/dashboard"}>
+                            {/* <Image
+                                className="w-11 rounded-full cursor-pointer"
+                                src={user.photoURL}
+                                alt=""
+                                width={50}
+                                height={50}
+                            /> */}
+                            <img
+                                className="w-11 rounded-full cursor-pointer"
+                                src={user.photoURL}
+                                alt=" "
+                            />
+                        </Link>
+                    </div>
+                )}
+            </ul>
+        </nav>
+    );
+}
---
+++ src/pages/post.js
@@ -0,0 +1,60 @@
+import auth, { db } from "../../utils/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { async } from "@firebase/util";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+
+export default function Post() {
+    const [post, setPost] = useState({
+        description: "",
+    });
+
+    const [user, loading] = useAuthState(auth);
+
+    //submitting post
+    const submitPost = async (e) => {
+        e.preventDefault();
+
+        //making a new post
+        const collectionRef = collection(db, "posts");
+        await addDoc(collectionRef, {
+            ...post,
+            timestamp: serverTimestamp(),
+            user: user.uid,
+            avatar: user.photoURL,
+            username: user.displayName,
+        });
+    };
+
+    return (
+        <div className="my-20 p-12 shadow-lg rounded-lg max-w-md mx-auto">
+            <form onSubmit={submitPost}>
+                <h1 className="text-2xl font-bold">Create a new post</h1>
+                <div className="py-2">
+                    <h3 className="txt-lg font-medium py-2">Description</h3>
+                    <textarea
+                        value={post.decription}
+                        onChange={(e) =>
+                            setPost({ ...post, description: e.target.value })
+                        }
+                        className="bg-gray-800 h-48 w-full text-white rounded-lg p-2"
+                    ></textarea>
+                    <p
+                        className={`text-cyan-600 font-medium text-sm ${
+                            post.description.length > 300 ? "text-red-600" : ""
+                        }`}
+                    >
+                        {post.description.length}/300
+                    </p>
+                </div>
+                <button
+                    type="submit"
+                    className="w-full bg-cyan-600 text-white font-medium p-2 my-2 rounded-lg text-sm"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    );
+}
